ae414d62ccf6ff4266cfd3bd3c8f4cf9b9ca2ef5
RichardGo9000 <71149500+RichardGo9000@users.noreply.github.com>
2021-01-04 13:03:07 -0700

    Delete Subtraction.d.ts
    fuck

--- js/problems/Subtraction.d.ts
+++
@@ -1,9 +0,0 @@
-import Problem from '../Problem.js';
-export declare class SubtractionProblem extends Problem {
-    constructor();
-    mathFn(digitCountA: 1 | 2 | 3, digitCountB: 1 | 2 | 3): {
-        question: string;
-        answer: number;
-    };
-}
-export default SubtractionProblem;
