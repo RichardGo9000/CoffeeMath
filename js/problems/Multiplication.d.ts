8260ba5044be8ff355665724c50c3ecef99715f1
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-03-03 09:45:07 -0700

    Delete Multiplication.d.ts
    Updating code base to a working PWA

--- js/problems/Multiplication.d.ts
+++
@@ -1,9 +0,0 @@
-import Problem from '../Problem.js';
-export declare class MultiplicationProblem extends Problem {
-    constructor();
-    mathFn(digitCountA: 1 | 2 | 3, digitCountB: 1 | 2 | 3): {
-        question: string;
-        answer: number;
-    };
-}
-export default MultiplicationProblem;
