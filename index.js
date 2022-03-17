7ea048a84c2b46e6499b2285213e86d420a0cfee
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-03-17 09:48:58 -0600

    Update index.js
    - [ ] PWA specific functions have been moved to their own function to easily enable/disable
    - [ ] event listeners have been added to topics category of settings menu.

--- index.js
+++ index.js
@@ -13,48 +13,94 @@ import Quiz from './js/Quiz.js';
 console.log("Calling Quiz()");
 new Quiz().init();
 
-//PWA Functionality
-//*****************
+function enablePWAFunctionality() {
+    // registers service worker
+    if ('serviceWorker' in navigator) {
+        //            '../sw.js'
+        navigator.serviceWorker
+            .register('sw.js')
+            .then(() => {
+                console.log('Service worker registered');
+            });
+    }
+    // Handle Install Prompt
+    let deferredPrompt;
+    const addBtn = document.querySelector('.installBtn'); //refactor to install button
 
-// registers service worker
-if ('serviceWorker' in navigator) {
-    //            '../sw.js'
-    navigator.serviceWorker
-        .register('sw.js')
-        .then(() => {
-            console.log('Service worker registered');
-        });
-}
-
-// Handle Install Prompt
-let deferredPrompt;
-const addBtn = document.querySelector('.installBtn');//refactor to install button
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    //prevent chrom 67 & earlier from automatically showing prompt
-    e.preventDefault();
-    //stash the event so it can be triggered later
-    deferredPrompt = e;
-    // addBtn.style.display = 'block';
-    addBtn.addEventListener('click', () => {
-        // addBtn.style.display = 'none';
-        //show the prompt
-        deferredPrompt.prompt();
-        //wait for user to decide
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted A2HS prompt');
-            } else {
-                console.log('User dismissied A2HS prompt');
-            }
-            deferredPrompt = null;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        //prevent chrom 67 & earlier from automatically showing prompt
+        e.preventDefault();
+        //stash the event so it can be triggered later
+        deferredPrompt = e;
+        // addBtn.style.display = 'block';
+        addBtn.addEventListener('click', () => {
+            // addBtn.style.display = 'none';
+            //show the prompt
+            deferredPrompt.prompt();
+            //wait for user to decide
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted A2HS prompt');
+                } else {
+                    console.log('User dismissied A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
         });
     });
+}
+// enablePWAFunctionality();
+
+
+// Listen for settings menu changes
+
+//Addition
+document.getElementById('topicAddition').addEventListener('click', (e) => {
+    console.log('Addition ' + e);
+});
+//Subtraction
+document.getElementById('topicSubtraction').addEventListener('click', (e) => {
+    console.log('Subtraction ' + e);
+});
+//Division
+document.getElementById('topicDivision').addEventListener('click', (e) => {
+    console.log('Division ' + e);
+});
+//Multiplication
+document.getElementById('topicMultiplication').addEventListener('click', (e) => {
+    console.log('Multiplication ' + e);
+});
+//Factorial
+document.getElementById('topicFactorial').addEventListener('click', (e) => {
+    console.log('Factorial ' + e);
+});
+//Percentage
+document.getElementById('topicPercentage').addEventListener('click', (e) => {
+    console.log('Percentage ' + e);
+});
+//Exponent
+document.getElementById('topicExponent').addEventListener('click', (e) => {
+    console.log('Exponent ' + e);
+});
+//Negatives
+document.getElementById('topicNegatives').addEventListener('click', (e) => {
+    console.log('Negatives ' + e);
+});
+//Fractions
+document.getElementById('topicFractions').addEventListener('click', (e) => {
+    console.log('Fractions ' + e);
+});
+//Decimals
+document.getElementById('topicDecimals').addEventListener('click', (e) => {
+    console.log('Decimals ' + e);
+});
+//Logarithms
+document.getElementById('topicLogarithms').addEventListener('click', (e) => {
+    console.log('Logarithms ' + e);
 });
 
-
 // Set up Settings DB
-const dataLayer = async() => {
+const dataLayer = async () => {
     const coffeeDB = "CoffeeMathDB";
 
     localforage.setItem('updateLevel', 0);
@@ -65,6 +111,6 @@ const dataLayer = async() => {
 }
 
 //IIFE
-(async() => {
+(async () => {
     await dataLayer()
 })()
