dd7f5a2bd9bc1c4cf33f52156a969a00c2a8c909
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-03-18 18:53:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// Main Web App
-//*************
-
-console.log('Javascript Active');
-
-
-
-
-// Present Quiz
 import localforage from 'https://cdn.skypack.dev/pin/localforage@v1.9.0-Ua6HaBzFSvfonEYhVE2t/min/localforage.js';
 import Quiz from './js/Quiz.js';
 
+// Main Web App
+//*************
+
+// Present Quiz
+
 console.log("Calling Quiz()");
 new Quiz().init();
+
 
 function enablePWAFunctionality() {
     // registers service worker
@@ -51,66 +48,128 @@ function enablePWAFunctionality() {
 }
 enablePWAFunctionality();
 
-
-// Listen for settings menu changes
-
-//Addition
-document.getElementById('topicAddition').addEventListener('click', (e) => {
-    console.log('Addition ' + e);
-});
-//Subtraction
-document.getElementById('topicSubtraction').addEventListener('click', (e) => {
-    console.log('Subtraction ' + e);
-});
-//Division
-document.getElementById('topicDivision').addEventListener('click', (e) => {
-    console.log('Division ' + e);
-});
-//Multiplication
-document.getElementById('topicMultiplication').addEventListener('click', (e) => {
-    console.log('Multiplication ' + e);
-});
-//Factorial
-document.getElementById('topicFactorial').addEventListener('click', (e) => {
-    console.log('Factorial ' + e);
-});
-//Percentage
-document.getElementById('topicPercentage').addEventListener('click', (e) => {
-    console.log('Percentage ' + e);
-});
-//Exponent
-document.getElementById('topicExponent').addEventListener('click', (e) => {
-    console.log('Exponent ' + e);
-});
-//Negatives
-document.getElementById('topicNegatives').addEventListener('click', (e) => {
-    console.log('Negatives ' + e);
-});
-//Fractions
-document.getElementById('topicFractions').addEventListener('click', (e) => {
-    console.log('Fractions ' + e);
-});
-//Decimals
-document.getElementById('topicDecimals').addEventListener('click', (e) => {
-    console.log('Decimals ' + e);
-});
-//Logarithms
-document.getElementById('topicLogarithms').addEventListener('click', (e) => {
-    console.log('Logarithms ' + e);
-});
-
 // Set up Settings DB
 const dataLayer = async () => {
-    const coffeeDB = "CoffeeMathDB";
 
-    localforage.setItem('updateLevel', 0);
+        const initDB = async () => {
+            const coffeeDB = "CoffeeMathDB";
+            localforage.config({
+                name: 'CoffeeMath',
+                storeName: 'CoffeeMathSettings'
+            });
+        }
+        //IIFE
+        (async () => {
+            await initDB()
+        })()
 
-    let updateLevel = null;
-    updateLevel = await localforage.getItem('updateLevel');
-    console.log("Update Level: " + updateLevel);
+        let appUpdateLevel = await localforage.getItem('appUpdateLevel');
+        console.log('Update Level: ' + appUpdateLevel);
+
+        if (appUpdateLevel == null) {
+            const saveDefaultSettings = async () => {
+                localforage.setItem('appUpdateLevel', 50);
+                localforage.setItem('topicAddition', true);
+                localforage.setItem('topicSubtraction', true);
+                localforage.setItem('topicMultiplication', true);
+                localforage.setItem('topicDivision', true);
+                localforage.setItem('topicFactorial', false);
+                localforage.setItem('topicPercentage', false);
+                localforage.setItem('topicExponent', false);
+                localforage.setItem('topicNegatives', false);
+                localforage.setItem('topicFractions', false);
+                localforage.setItem('topicDecimals', false);
+                localforage.setItem('topicLogarithms', false);
+            }
+            //IIFE
+            (async () => {
+                await saveDefaultSettings()
+            })()
+        } else {
+            (async () => {
+                await getSettingsFromDB()
+            })()
+        }
+
+
+    }
+    (async () => {
+        await dataLayer()
+    })()
+
+// Listen for settings menu changes
+function addMenuEventListeners() {
+
+
+    //Addition
+    document.getElementById('topicAddition').addEventListener('click', (e) => {
+        console.log('Addition ' + e);
+        localforage.setItem('topicAddition', document.getElementById('topicAddition').checked);
+    });
+    //Subtraction
+    document.getElementById('topicSubtraction').addEventListener('click', (e) => {
+        console.log('Subtraction ' + e);
+        localforage.setItem('topicSubtraction', document.getElementById('topicSubtraction').checked);
+    });
+    //Division
+    document.getElementById('topicDivision').addEventListener('click', (e) => {
+        console.log('Division ' + e);
+        localforage.setItem('topicDivision', document.getElementById('topicDivision').checked);
+
+    });
+    //Multiplication
+    document.getElementById('topicMultiplication').addEventListener('click', (e) => {
+        console.log('Multiplication ' + e);
+        localforage.setItem('topicMultiplication', document.getElementById('topicMultiplication').checked);
+    });
+    //Factorial
+    document.getElementById('topicFactorial').addEventListener('click', (e) => {
+        console.log('Factorial ' + e);
+        localforage.setItem('topicFactorial', document.getElementById('topicFactorial').checked);
+    });
+    //Percentage
+    document.getElementById('topicPercentage').addEventListener('click', (e) => {
+        console.log('Percentage ' + e);
+        localforage.setItem('topicPercentage', document.getElementById('topicPercentage').checked);
+    });
+    //Exponent
+    document.getElementById('topicExponent').addEventListener('click', (e) => {
+        console.log('Exponent ' + e);
+        localforage.setItem('topicExponents', document.getElementById('topicExponent').checked);
+    });
+    //Negatives
+    document.getElementById('topicNegatives').addEventListener('click', (e) => {
+        console.log('Negatives ' + e);
+        localforage.setItem('topicNegatives', document.getElementById('topicNegatives').checked);
+    });
+    //Fractions
+    document.getElementById('topicFractions').addEventListener('click', (e) => {
+        console.log('Fractions ' + e);
+        localforage.setItem('topicFractions', document.getElementById('topicFractions').checked);
+    });
+    //Decimals
+    document.getElementById('topicDecimals').addEventListener('click', (e) => {
+        console.log('Decimals ' + e);
+        localforage.setItem('topicDecimals', document.getElementById('topicDecimals').checked);
+    });
+    //Logarithms
+    document.getElementById('topicLogarithms').addEventListener('click', (e) => {
+        console.log('Logarithms ' + e);
+        localforage.setItem('topicLogarithms', document.getElementById('topicLogarithms').checked);
+    });
 }
+addMenuEventListeners();
 
-//IIFE
-(async () => {
-    await dataLayer()
-})()
+const getSettingsFromDB = async () => {
+    document.getElementById('topicAddition').checked = await localforage.getItem('topicAddition');
+    document.getElementById('topicSubtraction').checked = await localforage.getItem('topicSubtraction');
+    document.getElementById('topicMultiplication').checked = await localforage.getItem('topicMultiplication');
+    document.getElementById('topicDivision').checked = await localforage.getItem('topicDivision');
+    document.getElementById('topicFactorial').checked = await localforage.getItem('topicFactorial');
+    document.getElementById('topicPercentage').checked = await localforage.getItem('topicPercentage');
+    document.getElementById('topicExponent').checked = await localforage.getItem('topicExponents');
+    document.getElementById('topicNegatives').checked = await localforage.getItem('topicNegatives');
+    document.getElementById('topicFractions').checked = await localforage.getItem('topicFractions');
+    document.getElementById('topicDecimals').checked = await localforage.getItem('topicDecimals');
+    document.getElementById('topicLogarithms').checked = await localforage.getItem('topicLogarithms');
+}
