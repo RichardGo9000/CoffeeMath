600bea85b3cdc9bd7c96184691dcb792552472bd
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-01 16:50:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import CoffeeMath from './js/class/App.js';
+import CoffeeMath from './js/class/App.js';  //import app class
 
-const app = new CoffeeMath();
+const app = new CoffeeMath();  //instantiate new instance of coffeemath
 // To turn on logging add true to the fn below: I.e. `await app.init(true);`
-await app.init();
+await app.init();  //init app with asyncronous javascript
 
