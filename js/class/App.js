bac337d14113c4e46523594209a319dfee9223c1
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-02 09:23:34 -0600

    Update App.js

--- js/class/App.js
+++ js/class/App.js
@@ -27,36 +27,36 @@ export default class CoffeeMath { //export coffeemath class
     this.quiz = null;  //sets quiz as the 3rd class attribute.
   }
 
-  async init(debug = false){
+  async init(debug = false){  //create async function that has optional debuging, this runs when object.init is ran
     // Init user
-    this.user = new User();
+    this.user = new User();  //init a new user object
     // Load user preferences
-    await this.user.load(this.localDB, debug);
+    await this.user.load(this.localDB, debug);  //pass the localDB objectect from above to the user load function
     // If we want to debug lets log this
-    if (debug) console.log('[CoffeeMath:Init:Debug]', this);
+    if (debug) console.log('[CoffeeMath:Init:Debug]', this);  //log a identifier for the coffeemath object if debugging
     // Load the preference menu state
-    this.loadPreferenceMenuState(debug);
+    this.loadPreferenceMenuState(debug);  //? load a function that loops through the topics and sets the checkmark status of each
     // Attach listeners
-    this.attachListeners(debug);
+    this.attachListeners(debug); //?load a function that loops through each id in the topic list and assigns an event listener for the checkbox
     // Start the Quiz
-    this.quiz = new Quiz(this.user, debug)
+    this.quiz = new Quiz(this.user, debug)//once the settings menu is set to reflect the db and event listeners are set, then present the quiz
   }
 
-  loadPreferenceMenuState(debug = false){
-    if (debug) console.log('[CoffeeMath:loadPreferenceMenuState]', this.user.topics);
-    topics.forEach(id => {
-      document.getElementById(`topic${id}`).checked = this.user.topics[id.toLowerCase()]
-    })
-  }
+  loadPreferenceMenuState(debug = false){  //this takes the settings in the topics array and loops through them setting the checkboxes to reflect the data in the array
+    if (debug) console.log('[CoffeeMath:loadPreferenceMenuState]', this.user.topics);  //log the topics array to console if debugging
+    topics.forEach(id => {  //loop through each topic in the topics array with a foreach loop
+      document.getElementById(`topic${id}`).checked = this.user.topics[id.toLowerCase()]  //use document.getelementbyid to select each checkbox then set its checked status according to data in the array
+    }) //close foreach loop
+  }//close loadPreferencesMenuState function
 
-  attachListeners(debug = false){
-    topics.forEach(id => {
-      document.getElementById(`topic${id}`).addEventListener('click', (e) => {
-        if (debug) console.log('[CoffeeMath:AttachListeners]', `topic${id} ${e.target.checked ? 'checked' : 'unchecked'}`);
-        this.user.topics[id.toLowerCase()] = e.currentTarget.checked
-        if (debug) console.log('[CoffeeMath:AttachListeners]', this.user.topics);
-        this.user.save(this.localDB);
-      })
-    })
-  }
-}
+  attachListeners(debug = false){ //loop though topics and create an event listener for each one
+    topics.forEach(id => { //loop through each topic in topic array 
+      document.getElementById(`topic${id}`).addEventListener('click', (e) => { //select each attribute by id and add event listener
+        if (debug) console.log('[CoffeeMath:AttachListeners]', `topic${id} ${e.target.checked ? 'checked' : 'unchecked'}`); //log the status of each checkbox if debugging
+        this.user.topics[id.toLowerCase()] = e.currentTarget.checked //set the cvalue in the topics array to match the checkbox when checkbox is clicked
+        if (debug) console.log('[CoffeeMath:AttachListeners]', this.user.topics); //log the topics array
+        this.user.save(this.localDB);  //store array in localdb using function for the user class
+      })  //close the event listener code block
+    }) //close the foreach loop through the topics array
+  }//close the attachListeners function
+}  //close the class definition
