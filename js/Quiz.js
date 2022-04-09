3d7df4ba9081736b3e100c3a87f35d477d883496
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-09 09:43:07 -0600

    Update Quiz.js

--- js/Quiz.js
+++ js/Quiz.js
@@ -96,111 +96,110 @@ export class Quiz { //open definition of class to export as quiz
     selectRandomProblem() {  //open select random problem
         let random = Math.floor(Math.random() * this.possibleQuestions.length);  //choos a random number that is within the length of the possibleQuestions array
         let nextQuestion = this.possibleQuestions[random]; //set next question as a random number from the possible questions array 
-        let problemResponse;
-        switch (nextQuestion) {
-            case "addition": {
-                this.problem = new AdditionProblem();
-                problemResponse = this.problem.mathFn(2, 2);
-                break;
-            }
-            case "subtraction": {
-                this.problem = new SubtractionProblem();
-                problemResponse = this.problem.mathFn(2, 2);
-                break;
-            }
-            case "multiplication": {
-                this.problem = new MultiplicationProblem();
-                problemResponse = this.problem.mathFn(1, 2);
-                break;
-            }
-            case "division": {
-                this.problem = new DivisionProblem();
-                problemResponse = this.problem.mathFn(2, 1);
-                break;
-            }
-            case "factorial": {
-                this.problem = new FactorialProblem();
-                if (this.problem instanceof FactorialProblem) {
-                    problemResponse = this.problem.mathFn();
-                }
-                break;
-            }
-            case "percentage": {
-                this.problem = new PercentageProblem();
-                if (this.problem instanceof PercentageProblem) {
-                    problemResponse = this.problem.mathFn();
-                }
-                break;
-            }
-            case "exponent": {
-                this.problem = new ExponentProblem();
-                if (this.problem instanceof ExponentProblem) {
-                    problemResponse = this.problem.mathFn();
-                }
-                break;
-            }
-        }
-        this.questionList.push(`${this.questionNumber}) ${problemResponse.question} = ${problemResponse.answer}`);
+        let problemResponse; //hold the correct answer to the poroblem
+        switch (nextQuestion) {   //create switch statement to select the next problem
+            case "addition": {  //choose addition topic
+                this.problem = new AdditionProblem();  //create new instance of addition problem
+                problemResponse = this.problem.mathFn(2, 2); //tell the question how many digitsw to use for each factor of the problem the set the correct anser here
+                break; exit switch statement
+            } //close case codeblock
+            case "subtraction": { //select subtraction topic
+                this.problem = new SubtractionProblem(); //create new instance of subtraction object
+                problemResponse = this.problem.mathFn(2, 2); //tell iottt to suubtract 2 2 digit numbers then set the anser here
+                break; //escape switch statement
+            } //close case codeblock
+            case "multiplication": { //select multiplication
+                this.problem = new MultiplicationProblem(); //create new instance of multiplication class
+                problemResponse = this.problem.mathFn(1, 2);//multiply a 2 digit number by a 1 digit number, set the answer here
+                break;//exit switch statement
+            }//close case code block
+            case "division": {  //select division topic
+                this.problem = new DivisionProblem(); //create new instance of division  class
+                problemResponse = this.problem.mathFn(2, 1); //multiply 1 digit by a 2 digit number
+                break;//exit switch statement
+            }//close case codeblock
+            case "factorial": {//select factorial topic, 
+                this.problem = new FactorialProblem(); //creat3e new object from factorialproblem class
+                if (this.problem instanceof FactorialProblem) {//??not sure why we are dchecking for instance of, this may need to be removed 
+                    problemResponse = this.problem.mathFn();//create new factorial problem and set correct answer here
+                }//close extranious if statement
+                break;//exit switch statement
+            }//close case code block
+            case "percentage": { //select percentage topic
+                this.problem = new PercentageProblem(); //create new object from percentage problem class
+                if (this.problem instanceof PercentageProblem) { //??not sure why we are checking for instance of
+                    problemResponse = this.problem.mathFn(); //generate new percentage problem and set correct answer here
+                }//close extranious if statement here
+                break;//exit switch case
+            }//close percentage ccase code block
+            case "exponent": { //select exponent topic
+                this.problem = new ExponentProblem(); //create new object from percentage problem class
+                if (this.problem instanceof ExponentProblem) {//??check for instance of, still not sur3e why
+                    problemResponse = this.problem.mathFn();  //create the new question and set correct answer herre
+                }// close extraneous if statement
+                break;//exiut switch statement
+            }//close exponment case code block
+        }//closer switch statement code block
+        this.questionList.push(`${this.questionNumber}) ${problemResponse.question} = ${problemResponse.answer}`); //add the details of this question to the question list fopr fgenerating a review at the end
         if (this.debug) console.log(`${this.questionNumber}) ${problemResponse.question} = ${problemResponse.answer}`); //DEBUGGING
-        this.questionNumber++;
-    }
-    checkAnswer() {
-        let submittedAnswer = parseFloat(this.answerElement.value);
+        this.questionNumber++;//increment the current question number
+    } //close selectRandomProblem
+    checkAnswer() { //create checkanser function
+        let submittedAnswer = parseFloat(this.answerElement.value);  //round the answer to a standardized fformat that is known to the student
         // submittedAnswer = submittedAnswer
-        if (this.debug) console.log({
-            submittedAnswer
-        });
-        if (this.debug) console.log({
-            answer: this.problem.answer
-        });
-        if (submittedAnswer === this.problem.answer) {
+        if (this.debug) console.log({  //log ther answer if debugging
+            submittedAnswer//submitted answer to be logged
+        });//close if debugging statement
+        if (this.debug) console.log({  //if debugging then klog correct answer too
+            answer: this.problem.answer  //correct answser to be logged
+        });//close debugging if statement
+        if (submittedAnswer === this.problem.answer) {  //actually check if submitted answer and correct answer match
             // Correct
 
-            this.trackResponse("c");
-            this.setupNextCard();
-        } else {
-            // 
-            this.trackResponse("i");
-        }
-    }
-    trackResponse(response = "c") {
+            this.trackResponse("c");//track a correct responnse
+            this.setupNextCard();  //clear the card for the next question
+        } else {//run code if answer is incorrect
+            this.trackResponse("i");// track that incorrect anser was givin
+        } //close answer checking if statement
+    }//close check answer functiuon
+    trackResponse(response = "c") { //take the tracked response and add it to the response tracker array 
         // add response type to responseTracker
-        if (response == "c") {
-            this.responseTrackingString = this.responseTrackingString + response;
-            this.responseTracker.push(this.responseTrackingString);
-            this.responseTrackingString = "";
-        } else if (response == "i") {
-            this.responseTrackingString = this.responseTrackingString + response;
-        }
-    }
+        if (response == "c") {//code block to use if correct answer
+            this.responseTrackingString = this.responseTrackingString + response; //track a stridng of responses for this particular answer
+            this.responseTracker.push(this.responseTrackingString); //add all the responses for this answeer to the tracking array
+            this.responseTrackingString = ""; //clear the tracking string for the next question, this may be extranious bnut maybe not
+        } else if (response == "i") {  //if incorrect the andd the wrong answer to the tracking array
+            this.responseTrackingString = this.responseTrackingString + response;  //appends wrong answer to tracking string
+        }//close response if statement
+    } //close trackresponse if statement
 
-    showOverview() {
-        let quizOverview = "";
-        let i = 0;
-        let correctCount = 0;
-        this.questionList.forEach((equation) => {
-            let rating;
-            if (this.responseTracker[i] == "c") {
-                rating = '<span class="green">&#x2713;</span>';
-                correctCount++;
-            } else {
-                rating = '<span class="red">x&nbsp;&nbsp;</span>';
-            }
-            quizOverview += `<p>${rating} ${equation}</p>`;
-            i += 1;
-        });
-        let percentCorrect = (correctCount / this.quizLength) * 100;
-        quizOverview = `<h1>${percentCorrect}%</h1>` + quizOverview;
-        quizOverview = `<div class="overviewCard" id="overview">
+    showOverview() { //define show overview function
+        let quizOverview = "";  //create emty string to hold overview
+        let i = 0;  //start a counter at 0
+        let correctCount = 0; //count the number of correct answers
+        this.questionList.forEach((equation) => { //loop through and display each question that was presented
+            let rating;//open a vcariable for the rating
+            if (this.responseTracker[i] == "c") {  //if correct assign class green to the html span to show the user positive reinfocement with a green color
+                rating = '<span class="green">&#x2713;</span>';//the acutal green checkmark to show correct answer
+                correctCount++;  //increment correct count
+            } else {  //else respond to incorrect answer
+                rating = '<span class="red">x&nbsp;&nbsp;</span>';  //prepenmd a red x to the question in the list
+            }//close if stement that added x or checkmark
+            quizOverview += `<p>${rating} ${equation}</p>`; //create a p tag with the rating response followed buy the question itself
+            i += 1; //increment counter so we can loop to next question
+        });//close for loop
+        let percentCorrect = (correctCount / this.quizLength) * 100; //deterrmin percentage of questions that where correct
+        quizOverview = `<h1>${percentCorrect}%</h1>` + quizOverview;//prepend that percentage toi the top of the overview
+        quizOverview = `<div class="overviewCard" id="overview">   
           ${quizOverview}
-          </div>`;
-        if (this.debug) console.log(quizOverview);
-        document.getElementById("app").innerHTML = quizOverview;
-    }
+          </div>`;//put the overview iun its own div
+        if (this.debug) console.log(quizOverview); //if debugging then log overview to console
+        document.getElementById("app").innerHTML = quizOverview;  //actually add overview card to the page
+    } //close show overview function
 
-    submitCard() {
-        if (this.debug) console.log("running this.checkAnswer()");
-        this.checkAnswer();
-    }
-}
-export default Quiz;
+    submitCard() {  //define submit function card
+        if (this.debug) console.log("running this.checkAnswer()");  //if debugging then log that we are about to check the answer
+        this.checkAnswer();  //actually call check answer function
+    } //close submit card fuiniction
+} //close the Quiz class definition
+export default Quiz; //export Quiz class as a module
