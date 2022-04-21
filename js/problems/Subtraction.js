3a787f6e2cfac9a4daa3ae2c81dc996b49ad660f
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-21 13:51:41 -0600

    Update Subtraction.js

--- js/problems/Subtraction.js
+++ js/problems/Subtraction.js
@@ -1,33 +1,33 @@
-import Problem from '../Problem.js';
-export class SubtractionProblem extends Problem {
-    constructor() {
-        super();
-    }
-    mathFn(digitCountA, digitCountB) {
-        let a;
-        let b;
-        if (digitCountA === 2) {
-            a = this.randomDoubleDigit();
-        }
-        else if (digitCountA === 3) {
-            a = this.randomTripleDigit();
-        }
-        else {
-            a = this.randomSingleDigit();
-        }
-        if (digitCountB == 2) {
-            b = this.randomDoubleDigit();
-        }
-        else if (digitCountB == 3) {
-            b = this.randomTripleDigit();
-        }
-        else {
-            b = this.randomSingleDigit();
-        }
-        this.answer = a - b;
-        this.question = `${a} - ${b}`;
-        this.display();
-        return { question: this.question, answer: this.answer };
-    }
-}
-export default SubtractionProblem;
+import Problem from '../Problem.js';  //import problem class
+export class SubtractionProblem extends Problem {  //open class definnition for subtraction problems
+    constructor() {  //open constructor
+        super();  //?? link this object to its parent class problem
+    }  //close constructor
+    mathFn(digitCountA, digitCountB) {  //open function that actually calculates the problem
+        let a;  //init var for first term
+        let b;  //iinit var for second term
+        if (digitCountA === 2) {  //if 2 digits then call randomDoubleDiugit
+            a = this.randomDoubleDigit();  //set output of randomDoubleDigit to a
+        }  //close if
+        else if (digitCountA === 3) {  //if set to 3 digits then call randomtripledigit
+            a = this.randomTripleDigit();  //set value of a to the output of randomTripleDigit
+        }  //close else ifif 
+        else {   //by default present random single digit
+            a = this.randomSingleDigit();  //set value of a to output of randomSingleDigit
+        } //close else statement
+        if (digitCountB == 2) {  //if set to 2 digits the call random double digit
+            b = this.randomDoubleDigit();  //set value of b to output from randomDoubleDigit
+        }  //close if
+        else if (digitCountB == 3) {  //if digit counbt is set to 3 then get random triple digit
+            b = this.randomTripleDigit();  //set value of b to output of randomTripleDigit
+        }  //close else if
+        else {  //by defuault provide random single digit
+            b = this.randomSingleDigit();  //set value of b to random single digit
+        }  //close else statement
+        this.answer = a - b;  //calculate the correct answer 
+        this.question = `${a} - ${b}`;  //fformat the question being asked to present to the user
+        this.display();  //display the question 
+        return { question: this.question, answer: this.answer };  //return the question and anser, not sure where this goes
+    }  //close mathfn
+}  //close subtraction problem class definition
+export default SubtractionProblem;  //exeport class so it can be used by other scripts
