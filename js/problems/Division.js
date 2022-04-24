147ad8f056ef073a5b239f797e940fc8d5a1b485
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-24 10:41:18 -0600

    Update Division.js

--- js/problems/Division.js
+++ js/problems/Division.js
@@ -1,33 +1,33 @@
-import Problem from '../Problem.js';
-export class DivisionProblem extends Problem {
-    constructor() {
-        super();
-    }
-    mathFn(digitCountA, digitCountB) {
-        let a;
-        let b;
-        if (digitCountA === 2) {
-            a = this.randomDoubleDigit();
-        }
-        else if (digitCountA === 3) {
-            a = this.randomTripleDigit();
-        }
-        else {
-            a = this.randomSingleDigit();
-        }
-        if (digitCountB == 2) {
-            b = this.randomDoubleDigit();
-        }
-        else if (digitCountB == 3) {
-            b = this.randomTripleDigit();
-        }
-        else {
-            b = this.randomSingleDigit();
-        }
-        this.answer = Math.round((a / b) * 1000) / 1000;
-        this.question = `${a} / ${b}`;
-        this.display();
-        return { question: this.question, answer: this.answer };
-    }
-}
-export default DivisionProblem;
+import Problem from '../Problem.js'; //import pronlem class
+export class DivisionProblem extends Problem {//open class definition for divisionproblem
+    constructor() {  //open constructor
+        super();  //reference the parent class
+    } //close class contructor
+    mathFn(digitCountA, digitCountB) {  //open mathfn definition
+        let a;  //create a space for the first term
+        let b;  //create a space for the second term
+        if (digitCountA === 2) {  //if first arg is 2, then set a to 2 digit number
+            a = this.randomDoubleDigit();  //set a to output of random double digit
+        }  //close if statement
+        else if (digitCountA === 3) {  //check if first term should be 3 digits
+            a = this.randomTripleDigit();  //set a to output of randomtripledigit
+        } //close else it
+        else {   //otherwise set a to the output of randomsingle digit
+            a = this.randomSingleDigit();  //set value of a to a random single digit
+        } //close else
+        if (digitCountB == 2) {  //if second arg is 2 then set second term to double digit
+            b = this.randomDoubleDigit();  //set value of b to random double digit
+        } //close if
+        else if (digitCountB == 3) {  //open else if, check if second arg is a 3
+            b = this.randomTripleDigit();  //set value of b to output of randomtripledigit
+        }  //close else if
+        else {  //else set second term to single digit
+            b = this.randomSingleDigit();  //set value of b to output of random single digit
+        } //close else
+        this.answer = Math.round((a / b) * 1000) / 1000;  //calculate the correct answer
+        this.question = `${a} / ${b}`;  //format the question to present to user
+        this.display();  //display question
+        return { question: this.question, answer: this.answer };  //return question and answer to present at overview
+    }//close mathfn definition
+}  //close class definition
+export default DivisionProblem; //export class to be used elsewhere
