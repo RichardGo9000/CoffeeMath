95b9047942ea9f91c1e6584566128efca307c4eb
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-24 10:49:35 -0600

    Update Addition.js

--- js/problems/Addition.js
+++ js/problems/Addition.js
@@ -1,33 +1,33 @@
-import Problem from '../Problem.js';
-export class AdditionProblem extends Problem {
-    constructor() {
-        super();
-    }
-    mathFn(digitCountA, digitCountB) {
-        let a;
-        let b;
-        if (digitCountA === 2) {
-            a = this.randomDoubleDigit();
-        }
-        else if (digitCountA === 3) {
-            a = this.randomTripleDigit();
-        }
-        else {
-            a = this.randomSingleDigit();
-        }
-        if (digitCountB == 2) {
-            b = this.randomDoubleDigit();
-        }
-        else if (digitCountB == 3) {
-            b = this.randomTripleDigit();
-        }
-        else {
-            b = this.randomSingleDigit();
-        }
-        this.answer = a + b;
-        this.question = `${a} + ${b}`;
-        this.display();
-        return { question: this.question, answer: this.answer };
-    }
-}
-export default AdditionProblem;
+import Problem from '../Problem.js';  //import problem class
+export class AdditionProblem extends Problem {  //open addition problem class definition
+    constructor() {  //open constructor
+        super();  //reference the parent class
+    }  //clost constructor
+    mathFn(digitCountA, digitCountB) {  //open mathfn definition
+        let a;  //create a space for the first term
+        let b;  //create a space for the second term
+        if (digitCountA === 2) {  //if first arg is 2 then make first term 2 digits
+            a = this.randomDoubleDigit();  //set value of a to a random double digit
+        }  //close if
+        else if (digitCountA === 3) {  //if first arg is 3 then get random triple digit
+            a = this.randomTripleDigit();  //set a to value of random triple digit
+        }  //close else if
+        else {  //open else
+            a = this.randomSingleDigit();  //set value of a to random single digit
+        }//close else
+        if (digitCountB == 2) {  //check if second arg is a2
+            b = this.randomDoubleDigit();  //set value of b to output of randomdoubledigit
+        }  //close if
+        else if (digitCountB == 3) {  //if second arg is 3 then get a triple digit
+            b = this.randomTripleDigit();  //set value of b to the output of random triple digit
+        }  //close else if 
+        else {  //open else
+            b = this.randomSingleDigit();  //set value of b to the output of random single digit
+        }  //close else
+        this.answer = a + b;   //calculate the correct answer
+        this.question = `${a} + ${b}`;  //format the answer to present to user
+        this.display();  //display the question
+        return { question: this.question, answer: this.answer };  //return the question and correct answer for use in generating overview
+    }//close mathfn definition
+}  //close  additionproblem definition
+export default AdditionProblem;  //export class to be used elsewhere
