5e1d4d24d7631a86b0efe55201447bf2e1d62d4f
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-04-24 10:34:08 -0600

    Update Exponent.js

--- js/problems/Exponent.js
+++ js/problems/Exponent.js
@@ -1,18 +1,19 @@
 
-import Problem from '../Problem.js';
-export class ExponentProblem extends Problem {
-    constructor() {
-        super();
-    }
-    mathFn() {
-        const possibleBases = [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const possiblePowers = [0, 1, 2, 2, 2, 2, 3, 3, 4];
+import Problem from '../Problem.js';  //import problem class
+export class ExponentProblem extends Problem {  //open exponent problem class definition
+    constructor() {  //open constructor
+        super();  //reference the parent class 
+    }//close the constructor
+    mathFn() {  //open mathfn definition
+        //perhaps random single digit should get an optional weighted outcome
+        const possibleBases = [0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10];  //define an array of posible base numbers to apply an exponent too, this can be updated to random single digit
+        const possiblePowers = [0, 1, 2, 2, 2, 2, 3, 3, 4];  //define array of possible exponents, this can be updated to randome single digit
         const base = possibleBases[Math.floor(Math.random() * possibleBases.length)]; //randomSingleDigit();
         const power = possiblePowers[Math.floor(Math.random() * possiblePowers.length)]; //randomSingleDigit();
-        this.answer = base ** power;
-        this.question = `${base} ^ ${power}`;
-        this.display();
-        return { question: this.question, answer: this.answer };
-    }
-}
-export default ExponentProblem;
+        this.answer = base ** power;//calculate the correct answer
+        this.question = `${base} ^ ${power}`;  //format the question for the user
+        this.display();//display question to user
+        return { question: this.question, answer: this.answer };  //return question and correct answer to display to user
+    }  //close mathfn
+}//close class definition
+export default ExponentPrblem;  //export class to be used by other scripts
