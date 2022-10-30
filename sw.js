0e8efb8731c961c101527677381719f39c7567db
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-10-30 12:43:51 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,11 +25,62 @@ self.addEventListener('install', (e) => {
     );
 });
 
-
+//Begin original faulty fetch event listener
+/*
 self.addEventListener('fetch', (e) => {
     console.log(e.request.url);
     e.respondWith(
         caches.match(e.request).then((response) => response || fetch(e.request)),
     );
 });
+*/
 
+//Begin first fetch request intercept replacement idea
+// This implementation is glitchy and does not fully work
+/*
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response),
+    );
+});
+*/
+//End first fetch request intercept replacement idea
+
+
+//service worker examples
+// https://gist.github.com/cferdinandi/6e4a73a69b0ee30c158c8dd37d314663
+
+//The following example if from https://www.thecodeship.com/web-development/guide-service-worker-pitfalls-best-practices/
+/*
+console.log('setting fetch event listener');
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.open(CoffeeCache).then((CACHE) => {
+            return cache.match(e.request).then((response) => {
+                cache.put(e.request, response.clone());
+                    return response;
+                })/*;*/
+            });
+        })
+    );
+});
+*/
+
+
+// The following is an example from https://web.dev/learn/pwa/serving/
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request).then(cachedResponse => {
+         const networkFetch = fetch(event.request).then(response => {
+           // update the cache with a clone of the network response
+           caches.open("pwa-assets").then(cache => {
+               cache.put(event.request, response.clone());
+           });
+         });
+         // prioritize cached response over network
+         return cachedResponse || networkFetch;
+     }
+   )
+  )
+});
