f7c099fe0438c9eec38edf2494526b4b43a0c6c8
Richard Gorbet <71149500+RichardGo9000@users.noreply.github.com>
2022-11-03 10:01:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import Quiz from './js/Quiz.js';
 
 function enablePWAFunctionality() {
     // registers service worker
+    //consider running this after page has fully loaded, also consider having a list of minimum assets to run that are cached first then, optional assets that cache after that
     if ('serviceWorker' in navigator) {
         //            '../sw.js'
         navigator.serviceWorker
